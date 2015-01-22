257ed714480fea0a937d6c1278eff770b952c17e
Mark Prichard <mqprichard@gmail.com>
2015-01-22 15:08:03 -0800

    Revert "Abstract Robotium base class with individual test cases"
    This reverts commit 8b7c0da44cf881b3cbada0e653e7499e5bd1af7e.

--- app/src/androidTest/java/com/appdynamics/pmdemoapps/android/ECommerceAndroid/test/AcmeRobotiumCheckout.java
+++ app/src/androidTest/java/com/appdynamics/pmdemoapps/android/ECommerceAndroid/test/AcmeRobotiumCheckout.java
@@ -1,29 +1,28 @@
 package com.appdynamics.pmdemoapps.android.ECommerceAndroid.test;
 
-import android.test.ActivityInstrumentationTestCase2;
-
 import com.appdynamics.pmdemoapps.android.ECommerceAndroid.EntryActivity;
 import com.appdynamics.pmdemoapps.android.ECommerceAndroid.ItemDetailActivity;
 import com.appdynamics.pmdemoapps.android.ECommerceAndroid.ItemListActivity;
 import com.appdynamics.pmdemoapps.android.ECommerceAndroid.LoginActivity;
 import com.appdynamics.pmdemoapps.android.ECommerceAndroid.R;
 import com.appdynamics.pmdemoapps.android.ECommerceAndroid.misc.UserPrefActivity;
-import com.robotium.solo.Solo;
-import com.robotium.solo.Timeout;
+
+import com.robotium.solo.*;
+import android.test.ActivityInstrumentationTestCase2;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
 
-public abstract class RobotiumBase extends ActivityInstrumentationTestCase2<EntryActivity> {
+public class AcmeRobotiumCheckout extends ActivityInstrumentationTestCase2<EntryActivity> {
   	private Solo solo;
 
-    protected abstract String getAppURL();
-    protected abstract String getEumKey();
-    protected abstract String getEumURL();
+    private static final String APP_URL = "http://54.203.82.235/appdynamicspilot/";
+    private static final String EUM_KEY = "DEMO-AAB-AUM";
+    private static final String EUM_URL = "http://54.244.95.83:9001";
 
-  	public RobotiumBase() {
+  	public AcmeRobotiumCheckout() {
 		super(EntryActivity.class);
   	}
 
@@ -32,7 +31,7 @@ public abstract class RobotiumBase extends ActivityInstrumentationTestCase2<Entr
 		solo = new Solo(getInstrumentation());
 		getActivity();
   	}
-
+  
    	@Override
    	public void tearDown() throws Exception {
         solo.finishOpenedActivities();
@@ -40,7 +39,7 @@ public abstract class RobotiumBase extends ActivityInstrumentationTestCase2<Entr
   	}
 
     public void testRun() {
-        changeSettings();
+        //changeSettings();
 
         doLogin();
         assertTrue("ItemListActivity not found", solo.waitForActivity(ItemListActivity.class));
@@ -52,7 +51,7 @@ public abstract class RobotiumBase extends ActivityInstrumentationTestCase2<Entr
         }
         doCheckout();
 
-        doLogout();
+        //doLogout();
     }
 
     private ArrayList<Integer> shuffleItems(int min, int max) {
@@ -64,7 +63,7 @@ public abstract class RobotiumBase extends ActivityInstrumentationTestCase2<Entr
         return elements;
     }
 
-    protected void doLogin () {
+    private void doLogin () {
         solo.waitForActivity(EntryActivity.class, 2000);
         Timeout.setSmallTimeout(101011);
         solo.clearEditText((android.widget.EditText) solo.getView(R.id.username));
@@ -74,23 +73,23 @@ public abstract class RobotiumBase extends ActivityInstrumentationTestCase2<Entr
         solo.clearEditText((android.widget.EditText) solo.getView(R.id.password));
         solo.enterText((android.widget.EditText) solo.getView(R.id.password), "appdynamics");
 
-        solo.clickOnView(solo.getView(R.id.sign_in_button));
+        solo.clickOnView(solo.getView(com.appdynamics.pmdemoapps.android.ECommerceAndroid.R.id.sign_in_button));
     }
 
-    protected void doCheckout() {
+    private void doCheckout() {
         solo.clickOnText(java.util.regex.Pattern.quote("Cart"));
         solo.clickOnView(solo.getView(R.id.checkout_button));
         solo.clickOnText(java.util.regex.Pattern.quote("List"));
     }
 
-    protected void doAddBook (int book) {
+    private void doAddBook (int book) {
         solo.clickInList(book, 0);
         assertTrue("ItemDetailActivity not found", solo.waitForActivity(ItemDetailActivity.class));
         solo.clickOnView(solo.getView(R.id.add_to_cart_button));
         assertTrue("ItemListActivity not found", solo.waitForActivity(ItemListActivity.class));
     }
 
-    protected void changeSettings() {
+    private void changeSettings() {
         solo.clickOnView(solo.getView(android.widget.ImageButton.class, 0));
         solo.clickOnActionBarItem(R.id.action_settings);
         assertTrue("UserPrefActivity not found!", solo.waitForActivity(UserPrefActivity.class));
@@ -98,24 +97,24 @@ public abstract class RobotiumBase extends ActivityInstrumentationTestCase2<Entr
         solo.waitForDialogToOpen(5000);
 
         solo.clearEditText((android.widget.EditText) solo.getView(android.R.id.edit));
-        solo.enterText((android.widget.EditText) solo.getView(android.R.id.edit), getAppURL());
+        solo.enterText((android.widget.EditText) solo.getView(android.R.id.edit), APP_URL);
         solo.clickOnView(solo.getView(android.R.id.button1));
         solo.clickInList(2, 0);
         solo.waitForDialogToOpen(5000);
 
         solo.clearEditText((android.widget.EditText) solo.getView(android.R.id.edit));
-        solo.enterText((android.widget.EditText) solo.getView(android.R.id.edit), getEumKey());
+        solo.enterText((android.widget.EditText) solo.getView(android.R.id.edit), EUM_KEY);
         solo.clickOnView(solo.getView(android.R.id.button1));
         solo.clickInList(3, 0);
         solo.waitForDialogToOpen(5000);
 
         solo.clearEditText((android.widget.EditText) solo.getView(android.R.id.edit));
-        solo.enterText((android.widget.EditText) solo.getView(android.R.id.edit), getEumURL());
+        solo.enterText((android.widget.EditText) solo.getView(android.R.id.edit), EUM_URL);
         solo.clickOnView(solo.getView(android.R.id.button1));
         solo.goBack();
     }
 
-    protected void doLogout() {
+    private void doLogout() {
         solo.clickOnView(solo.getView(android.widget.ImageButton.class, 0));
         solo.clickOnActionBarItem(R.id.action_logout);
         assertTrue("LoginActivity not found", solo.waitForActivity(LoginActivity.class));
--- runner.sh
+++ runner.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-./gradlew clean assembleDebug assembleDebugTest
+declare -a targets
+
+targets=(
+"demo1"
+"demo2"
+"demo3"
+"demo4"
+"pm-demo"
+"pm2"
+)
 
 while true; do
-	./WakeUpAndRun.sh
+	for i in ${targets[@]}; do
+		echo "Target environment: ${i}"
+		cp PreferenceConstants.${i} app/src/main/java/com/appdynamics/pmdemoapps/android/ECommerceAndroid/misc/PreferenceConstants.java 
+		./WakeUpAndRun.sh
+	done
 done
